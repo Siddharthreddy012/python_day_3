d60605b24ce6e77df12d4349b65f7305a553480b
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-06-28 22:49:40 +0530

    Error_indexing

---
+++ Task_8.py
@@ -0,0 +1,7 @@
+# we are writing error program 
+l = Patchouli
+print("Given string is :",l)
+print("showing H in the given string :",l[4])
+print("showing O in the given string :",l[-4])
+print("showing T in the given string :",l[-7])
+print("showing I in the given string :",l[8])
